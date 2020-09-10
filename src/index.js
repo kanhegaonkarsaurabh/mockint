e219cd72f12fe0a3cb795baa646d9bdae1a5957c
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2020-09-10 02:28:36 -0700

    added split pane for mock int session

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+// import App from "./App";
+import MockIntSession from './components/MockIntSession';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MockIntSession />
   </React.StrictMode>,
   rootElement
 );
