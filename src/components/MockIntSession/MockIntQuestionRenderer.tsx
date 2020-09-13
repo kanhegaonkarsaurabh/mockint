230d3a23de04a598d86de2b31c37972f9ed0f2a4
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2020-09-13 15:01:32 -0700

    checkpoint commit - added prettier

--- src/components/MockIntSession/MockIntQuestionRenderer.tsx
+++ src/components/MockIntSession/MockIntQuestionRenderer.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
+import React from "react";
+import ReactMarkdown from "react-markdown";
 import { Flex } from "@chakra-ui/core";
 import ChakraUIRenderer from "chakra-ui-markdown-renderer";
-import './MockIntQuestionRenderer.css';
+import "./MockIntQuestionRenderer.css";
 interface Props {
-  questionAsMdString: string 
+  questionAsMdString: string;
 }
 
 export const MockIntQuestionRenderer = (props: Props) => {
   return (
-    <Flex direction="column" pl={'20px'} pr={'20px'} pb={'20px'}>
-      <ReactMarkdown 
-        source={props.questionAsMdString} 
+    <Flex direction="column" pl={"20px"} pr={"20px"} pb={"20px"}>
+      <ReactMarkdown
+        source={props.questionAsMdString}
         renderers={ChakraUIRenderer()}
-        />
+      />
     </Flex>
-  )
-}
+  );
+};
--- src/components/MockIntSession/MockIntSession.tsx
+++ src/components/MockIntSession/MockIntSession.tsx
@@ -1,31 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import SplitPane from 'react-split-pane';
-import './MockIntSession.css';
-import { MockIntQuestionRenderer } from './MockIntQuestionRenderer';
-import mockQuestion from './mockQuestion.md';
+import React, { useEffect, useState } from "react";
+import SplitPane from "react-split-pane";
+import { jsx } from "@emotion/core";
+import styled from "@emotion/styled";
+import "./MockIntSession.css";
+import { MockIntQuestionRenderer } from "./MockIntQuestionRenderer";
+import mockQuestion from "./mockQuestion.md";
+import { Flex, Box, Heading } from "@chakra-ui/core";
 
-const MockIntSession : React.FunctionComponent<{ message: string }> = () => {
-  const [question, setQuestion] =  useState<string>('');
+const SplitPaneWrapper = styled(Box)({
+  height: "60%",
+  width: "100%",
+});
+
+const MockIntSession: React.FunctionComponent<{ message: string }> = () => {
+  const [question, setQuestion] = useState<string>("");
   useEffect(() => {
     fetch(mockQuestion)
-    .then(ques => ques.text())
-    .then(quesText => setQuestion(quesText));
+      .then((ques) => ques.text())
+      .then((quesText) => setQuestion(quesText));
   });
 
   return (
-    <SplitPane 
-      split="vertical"
-      minSize={300}
-      defaultSize={300}
-      className="primary"
-      allowResize={true}
-      primary="first"
-      pane1Style={{overflowY: 'auto'}}  // to get scrollable questions
-    >
-      <MockIntQuestionRenderer questionAsMdString={question}/>
-      <div>Using a Pane allows you to specify any constraints directly</div>
-    </SplitPane>
-  )
-}
+    <Flex direction="column">
+      <Box style={{ height: "70%" }}>
+        <Heading> Name of the session </Heading>
+      </Box>
+      <SplitPaneWrapper>
+        <SplitPane
+          split="vertical"
+          minSize={300}
+          defaultSize={300}
+          className="primary"
+          allowResize={true}
+          primary="first"
+          pane1Style={{ overflowY: "auto" }} // to get scrollable questions
+        >
+          <MockIntQuestionRenderer questionAsMdString={question} />
+          <div>Using a Pane allows you to specify any constraints directly</div>
+        </SplitPane>
+      </SplitPaneWrapper>
+    </Flex>
+  );
+};
 
 export default MockIntSession;
